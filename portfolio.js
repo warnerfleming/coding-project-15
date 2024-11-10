143dd5ea1752c6e4266cf049e009426daf36ca8d
warnerfleming <fleming14@usf.edu>
2024-11-10 11:49:37 -0500

    Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,11 @@
+//Task 2
+
+import {assets}
+ from "./asset.js"
+
+ 
+ export function calculatePortfolioValue() {
+    return assets.reduce((item, asset) => item + (asset.quantity * asset.price),0)
+ }
+
 
