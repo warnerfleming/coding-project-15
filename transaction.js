b7702db58ef4ee04ae1639003bed6ccda4ee1ae5
warnerfleming <fleming14@usf.edu>
2024-11-10 12:02:44 -0500

    Create transaction module with Transaction class

--- transaction.js
+++ transaction.js
@@ -1,1 +1,21 @@
+// Task 3
+export class Transaction {
+    constructor(assetId, type, quantity) {
+        this.type = type;
+        this.quantity = quantity;
+        this.asset = getAssetById(assetId);
+        
+        this.inputTransaction();
+    }
+
+    inputTransaction() {
+        if (this.type === "buy") {
+            this.asset.quantity += this.quantity;
+        } else if (this.type === "sell") {
+            if (this.asset.quantity < this.quantity) {
+                throw new Error(`Insufficient quantity for sale of ${this.asset.name}`);
+            }
+            this.asset.quantity -= this.quantity;
+    }}
+}
 
